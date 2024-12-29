89d5a4d5ae519edd0e274d8f0e802955b6522b2d
ZikoZaki <ghostthe935@gmail.com>
2024-12-29 12:32:00 +0100

    mini-projet

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, url_for, redirect, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route("/submit-form", methods=['POST'])
+def submit_form():
+    name = request.form['name']
+    email = request.form['email']
+    msg = request.form['msg']
+    print(f'Name: {name}\nEmail: {email}\nmsg: {msg}\n')
+    return render_template("submit-success.html", name=name, email=email, msg=msg)
+
+if __name__ == '__main__':
+    app.run(debug = True)
---
+++ static/js/script.js
@@ -0,0 +1,6 @@
+
+let ham = document.getElementById("hamburger");
+let nav = document.getElementById("nav-bar");
+function show_tab(){
+    let navclass = nav.classList.toggle("show");
+}
