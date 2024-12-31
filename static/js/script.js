df795624384cfdabde82d9ab91ceeb1e088eedf0
Zakaria_Zaki <zaakizakaria12@gmail.com>
2024-12-31 22:03:32 +0100

    a lot of them

--- static/js/script.js
+++ static/js/script.js
@@ -4,3 +4,6 @@ let nav = document.getElementById("nav-bar");
 function show_tab(){
     let navclass = nav.classList.toggle("show");
 }
+const root = document.documentElement;
+const currentTransform = getComputedStyle(root).getPropertyValue('--Htransform');
+root.style.setProperty('--Htransform', -560);
